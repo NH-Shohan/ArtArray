e9406dee02416587d0b13c476cdc5b51562faadb
NH-Shohan <nh.shohan6@gmail.com>
2023-12-25 01:20:29 +0600

    button and navbar component added and design style added from figma

---
+++ app/components/Button.jsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Button({
+  text,
+  className,
+  outline,
+  fill,
+  src,
+  width,
+  height,
+  href,
+}) {
+  return (
+    <Link href={href}>
+      <button
+        className={
+          outline
+            ? `border border-gray bg-white py-2 px-8 rounded-lg hover:bg-gray hover:text-white transition-all flex items-center gap-1 ${className}`
+            : fill &&
+              `border border-gray py-2 px-8 rounded-lg bg-gray text-white ${className}`
+        }
+      >
+        {src && <Image src={src} alt="Image" width={width} height={height} />}
+        {text}
+      </button>
+    </Link>
+  );
+}
