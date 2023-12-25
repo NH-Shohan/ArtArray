21dd73b2c597d4df1c72ce9bda7bbdb95d4f99a9
NJ-Zerin <nusratzerin44@gmail.com>
2023-12-26 03:18:45 +0600

    fixed singin page

--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -1,3 +1,64 @@
+import Image from "next/image";
+import Link from "next/link";
+import Button from "@/app/components/Button";
+
 export default function SignIn() {
-  return <div>Sign in</div>;
+  return (
+    <div className="flex">
+      <div>
+        <form 
+        className="bg-light 
+        h-[419px] 
+        w-[491px] 
+        p-10 
+        rounded-xl 
+        mt-[80px]
+        ml-[100px]">
+        <h2 className="text-center
+         pb-7">Sign In</h2>
+
+          <label className="text-light-gray">
+            Email:
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            className="w-full px-4 py-2 border-none rounded mb-4"
+          />
+
+          <label className=" block text-light-gray">
+            Password:
+          </label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            required
+            className="w-full px-4 py-2 border-none rounded mb-10"
+          />
+          <h3 className="text-center"><Button  className="
+          h-[56px] 
+          w-[406px] 
+          mb-[10px]" 
+          text="Sign In"  fill href="/" /></h3>
+          <p>Don't have an account? <Link className="body bold" href="/auth/signup">Sign up</Link></p>
+        </form>
+      </div>
+      <div >
+      <Link href="/">
+            <Image
+              className="border-none
+              mt-[50px]"
+              src="/signin.svg"
+              alt="Sgn In Icon"
+              width={500}
+              height={500}
+            />
+          </Link>
+
+      </div>
+    </div>
+  );
 }
