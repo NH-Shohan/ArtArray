3a6f42bc44b4aa3264a0085536ba0b547bb2b060
NH-Shohan <nh.shohan6@gmail.com>
2023-12-27 14:28:30 +0600

    animation added to the admin routes

--- app/(admin)/layout.jsx
+++ app/(admin)/layout.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { AnimatePresence, motion } from "framer-motion";
 import { Roboto_Slab } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
@@ -11,73 +12,106 @@ function DashboardLayout({ children }) {
   const pathname = usePathname();
 
   return (
-    <section
-      className={`${roboto_Slab.className} grid grid-cols-12 gap-4 h-screen py-10`}
-    >
-      <div className="p-5 col-span-3 row-span-2 border border-light rounded-xl flex flex-col items-center relative">
-        <Image
-          className="rounded-full border-gray-light border-2"
-          src="/user.svg"
-          alt="User Image"
-          width={100}
-          height={100}
-          priority
-        />
-        <p className="mt-4 bold">Nahim Hossain Shohan</p>
+    <AnimatePresence>
+      <motion.div
+        key={pathname}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{
+          duration: 0.7,
+        }}
+        variants={{
+          initialState: {
+            opacity: 0,
+          },
+          animateState: {
+            opacity: 1,
+          },
+          exitState: {
+            opacity: 0,
+          },
+        }}
+        className={`${roboto_Slab.className} grid grid-cols-12 gap-4 h-screen py-10`}
+      >
+        <div className="p-5 col-span-3 row-span-2 border border-light rounded-xl flex flex-col items-center relative">
+          <Image
+            className="rounded-full border-gray-light border-2"
+            src="/user.svg"
+            alt="User Image"
+            width={100}
+            height={100}
+            priority
+          />
+          <p className="mt-4 bold">Nahim Hossain Shohan</p>
 
-        <hr className="border border-light w-full my-7" />
+          <hr className="border border-light w-full my-7" />
 
-        <Link
-          href={"/dashboard"}
-          className={`hover:bg-light text-gray-light hover:text-black w-full py-2 rounded-lg small text-left pl-4 transition-all ${
-            pathname === "/dashboard" && "active"
-          }`}
-        >
-          <button className={``}>dsahboard</button>
-        </Link>
-        <Link href={"/arts"} className="w-full">
-          <button
+          <Link
+            href={"/"}
             className={`hover:bg-light text-gray-light hover:text-black w-full py-2 rounded-lg small text-left pl-4 transition-all ${
-              pathname == "/arts" ? "active" : ""
+              pathname === "/" && "active"
             }`}
           >
-            Arts
-          </button>
-        </Link>
-        <Link href={"/users"} className="w-full">
-          <button
+            <button>Home</button>
+          </Link>
+          <Link
+            href={"/dashboard"}
             className={`hover:bg-light text-gray-light hover:text-black w-full py-2 rounded-lg small text-left pl-4 transition-all ${
-              pathname == "/users" ? "active" : ""
+              pathname === "/dashboard" && "active"
             }`}
           >
-            Users
-          </button>
-        </Link>
-
-        <div className="absolute bottom-0 w-full p-5">
-          <Link href={"/users"} className="w-full">
+            <button>dsahboard</button>
+          </Link>
+          <Link href={"/arts"} className="w-full">
             <button
-              className={`hover:bg-red-light text-red small-bold w-full py-2 rounded-lg small text-left pl-4 transition-all`}
+              className={`hover:bg-light text-gray-light hover:text-black w-full py-2 rounded-lg small text-left pl-4 transition-all ${
+                pathname == "/arts" ? "active" : ""
+              }`}
             >
-              Logout
+              Arts
             </button>
           </Link>
-        </div>
-      </div>
-      <div className="col-span-9 border border-light rounded-xl h-[60px] flex items-center pl-6">
-        <h3>
-          <span className="font-normal">ArtArray - </span>
-          <span>
-            {pathname.split("/")[1].charAt(0).toUpperCase() +
-              pathname.split("/")[1].slice(1)}
-          </span>
-        </h3>
-      </div>
+          <Link href={"/users"} className="w-full">
+            <button
+              className={`hover:bg-light text-gray-light hover:text-black w-full py-2 rounded-lg small text-left pl-4 transition-all ${
+                pathname == "/users" ? "active" : ""
+              }`}
+            >
+              Users
+            </button>
+          </Link>
 
-      <div className="col-span-9 border border-light rounded-xl h-[calc(100vh-140px)] w-full p-5 overflow-y-auto overflow-x-hidden scrollbar scrollbar-track-light scrollbar-thumb-gray-light scrollbar-thumb-rounded scrollbar-track-rounded scrollbar-w-[6px]">
-        {children}
-      </div>
-    </section>
+          <div className="absolute bottom-0 w-full p-5">
+            <Link href={"/users"} className="w-full">
+              <button
+                className={`hover:bg-red-light text-red small-bold w-full py-2 rounded-lg small text-left pl-4 transition-all`}
+              >
+                Logout
+              </button>
+            </Link>
+          </div>
+        </div>
+
+        <div className="col-span-9 border border-light rounded-xl h-[60px] flex items-center pl-6">
+          <h3>
+            <span className="font-normal">ArtArray - </span>
+            {pathname !== "/" ? (
+              <span>
+                {pathname.split("/")[1].charAt(0).toUpperCase() +
+                  pathname.split("/")[1].slice(1)}
+              </span>
+            ) : (
+              <span>Home</span>
+            )}
+          </h3>
+        </div>
+
+        <div className="col-span-9 border border-light rounded-xl h-[calc(100vh-140px)] w-full p-5 overflow-y-auto overflow-x-hidden scrollbar scrollbar-track-light scrollbar-thumb-gray-light scrollbar-thumb-rounded scrollbar-track-rounded scrollbar-w-[6px]">
+          {children}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
--- app/(admin)/page.jsx
+++ app/(admin)/page.jsx
@@ -1,9 +1,8 @@
-import Dashboard from "./dashboard/page";
-
-export default function Home() {
+export default function AdminHome() {
   return (
-    <div>
-      <Dashboard />
+    <div className="flex flex-col justify-center items-center h-full w-full">
+      <h3>Hello there! 👋🏼 Welcome to ArtArray</h3>
+      <h1 className="text-border">Nahim Hossain Shohan</h1>
     </div>
   );
 }
