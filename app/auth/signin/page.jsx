ee5c2e90c6ff2a26eb4dd4d8bb0271726fc14427
NH-Shohan <nh.shohan6@gmail.com>
2023-12-27 15:59:43 +0600

    signin page fixed

--- app/auth/signin/page.jsx
+++ app/auth/signin/page.jsx
@@ -1,55 +1,46 @@
+import Button from "@/app/components/Button";
 import Image from "next/image";
 import Link from "next/link";
-import Button from "@/app/components/Button";
 
 export default function SignIn() {
   return (
-  <div className="grid grid-cols-1 md:grid-cols-2 gap-1">
-  <div className="bg-light p-10 rounded-xl mt-20 ml-24">
-    <form className="grid grid-cols-1">
-      <h2 className="text-center pb-7">Sign in</h2>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-24 items-center h-[calc(100vh-60px)]">
+      <form className="bg-light p-10 rounded-xl h-fit">
+        <h2 className="text-center text-black">Sign in</h2>
 
-      <label className="block body small">Email</label>
-      <input
-        type="email"
-        id="email"
-        name="email"
-        placeholder="Enter your email"
-        required
-        className="w-full px-4 py-2 border-none rounded mb-4"
-      />
-
-      <label className="block body small">Password</label>
-      <input
-        type="password"
-        id="password"
-        name="password"
-        placeholder="Enter your password"
-        required
-        className="w-full px-4 py-2 border-none rounded mb-6"
-      />
-      <h3 className="text-center">
-        <Button
-          className="w-full mb-4"
-          text="Sign in"
-          fill
-          href="/"
+        <label className="text-gray-light small">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter email"
+          required
+          className="w-full px-4 py-3 rounded-lg mb-4 outline-none border border-gray-light"
         />
-      </h3>
-      <p>Already have an account?<Link className="body bold ml-2" href="/auth/signup">Sign up</Link></p>
-    </form>
-  </div>
-  <div className="flex items-center justify-center mt-24">
-    <Link href="/auth/signin">
-      <Image
-        src="/signin.svg"
-        alt="Sign In Icon"
-        width={500}
-        height={500}
-      />
-    </Link>
-  </div>
-</div>
 
+        <label className="text-gray-light small">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          placeholder="Enter password"
+          required
+          className="w-full px-4 py-3 rounded-lg mb-4 outline-none border border-gray-light"
+        />
+
+        <h3>
+          <Button className="w-full mb-2 mt-6" text="Sign in" fill href="/" />
+        </h3>
+
+        <p>
+          Already have an account?
+          <Link className="bold ml-1" href="/auth/signup">
+            Sign up
+          </Link>
+        </p>
+      </form>
+
+      <Image src="/signin.svg" alt="Sign In Icon" width={500} height={400} />
+    </div>
   );
 }
